82f1d0288e46c920cf4ccd610212f4e1dac20f2e
Timothy Cole <tim@timcole.me>
2021-03-20 22:26:15 -0400

    Does this build?

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,27 +1,6 @@
-import { createGlobalStyle, ThemeProvider } from "styled-components";
-import 'tailwindcss/tailwind.css'
-
-const GlobalStyle = createGlobalStyle`
-  body {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
-`;
-
-const theme = {
-  colors: {
-    primary: "#0070f3",
-  },
-};
+import "tailwindcss/tailwind.css";
+import "../styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return (
-    <>
-      <GlobalStyle />
-      <ThemeProvider theme={theme}>
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </>
-  );
+  return <Component {...pageProps} />;
 }
